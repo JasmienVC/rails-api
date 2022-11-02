64ac3c00ee8d384f77ae6f3622555afdb4d693f4
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-02 17:30:40 +0100

    improved style

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   it 'tests article object' do
-    article = FactoryBot.create(:article)
+    article = create(:article)
     expect(article.title).to eq 'Sample article'
     # expect(1).to be_positive ## _positive appends .positive? on tested number
   end
