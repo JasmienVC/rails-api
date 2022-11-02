2a987d52ac9607fc9ccb9ebd0910f5c2fc383dc1
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-02 17:20:11 +0100

    exemple test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'tests a number to be positive' do
+    expect(1).to be_positive # _positive appends .positive? on tested number
+  end
 end
