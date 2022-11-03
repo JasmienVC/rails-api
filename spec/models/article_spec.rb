96c4f112ec4448e5328f514ca55be6c2e5ad0925
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-03 11:43:52 +0100

    working on validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'tests article object' do
-    article = create(:article)
-    expect(article.title).to eq 'Sample article'
-    # expect(1).to be_positive ## _positive appends .positive? on tested number
+  describe '#validations' do
+    # prepend tests linked with specific instances with #
+    # prepend tests linked with class with .
+    let(:article) { build(:article) } # define article instance with let method to reuse
+    it 'tests if factory is valid' do
+      expect(article).to be_valid
+      # expect(1).to be_positive ## _positive appends .positive? on tested number
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
   end
 end
