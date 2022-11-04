65c73a4d7e9c10f4a1faa6f2705746b8b4edd060
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-04 10:24:42 +0100

    aggregate failures block

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe '/articles routes' do
   it 'routes to articles#index' do
+    aggregate_failures do
+      expect(get '/articles').to route_to('articles#index')
+      expect(get '/articles?page[number]=3').to route_to('articles#index', page: { number: 3 })
+    end
     # expect(get '/articles').to route_to(controller: 'articles', action: 'index')
-    expect(get '/articles?page[number]=3').to route_to('articles#index', page: { number: 3 })
   end
 end
