652014d68bd9af3c23777b83ef482fef677f807c
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-05 16:19:11 +0100

    optimized style

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :articles, only: [:index, :show]
+  resources :articles, only: %i[index show]
 end
