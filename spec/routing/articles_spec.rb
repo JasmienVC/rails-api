1cbbe986bfc915d33fa282e09ae8e25d250b663b
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-05 16:17:47 +0100

    test and route integrated

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe '/articles routes' do
     end
     # expect(get '/articles').to route_to(controller: 'articles', action: 'index')
   end
+
+  it 'routes to articles#show' do
+    expect(get 'articles/1').to route_to('articles#show', id: '1')
+  end
 end
