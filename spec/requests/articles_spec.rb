da3015fbbb7448e42396f28113eb154d9da4b8c5
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-07 14:18:21 +0100

    added pagination tests

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -33,5 +33,19 @@ RSpec.describe ArticlesController do
         eq([recent_article.id, older_article.id])
       )
     end
+
+    it 'paginates results' do
+      article_one, article_two, article_three = create_list(:article, 3)
+      get '/articles', params: { page: { number: 2, size: 1 } }
+      expect(json_data.length).to eq(1)
+      expect(json_data.first[:id]).to eq(article_two.id)
+    end
+
+    it 'contains pagination links in the response' do
+      article_one, article_two, article_three = create_list(:article, 3)
+      get '/articles', params: { page: { number: 2, size: 1 } }
+      expect(json[:links].length).to eq(5)
+      expect(json[:links].keys).to eq('first', 'prev', 'next', 'last', 'self')
+    end
   end
 end
