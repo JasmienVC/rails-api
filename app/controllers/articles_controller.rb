2d192136cf653db0796202c0e1add856c58abeeb
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-07 14:43:05 +0100

    index method articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,19 @@
 class ArticlesController < ApplicationController
   def index
     articles = Article.recent
-    render json: ArticleSerializer.new(articles), status: :ok
+    paginated_articles = paginator.call(
+      articles,
+      params: pagination_params,
+      base_url: request.url
+    )
+    render json: ArticleSerializer.new(paginated_articles.items), status: :ok
+  end
+
+  def paginator
+    JSOM::Pagination::Paginator.new
+  end
+
+  def pagination_params
+    params.permit![:page]
   end
 end
