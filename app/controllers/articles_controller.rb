7626e4569b6a4beefc3dd46d63df6c42958b814f
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-08 09:25:32 +0100

    DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,4 @@ class ArticlesController < ApplicationController
     paginated_articles = paginate(Article.recent)
     render_collection(paginated_articles)
   end
-
-  def paginator
-    JSOM::Pagination::Paginator.new
-  end
-
-  def pagination_params
-    params.permit![:page]
-  end
 end
