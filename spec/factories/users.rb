1bcd0f2e9712da749d40e7843f4b8caf72b7f1da
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-08 11:04:40 +0100

    user model validation tests added

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    login { "MyString" }
-    name { "MyString" }
-    url { "MyString" }
-    avatar_url { "MyString" }
-    provider { "MyString" }
+    sequence(:login) { |n| "jsmith#{n}" }
+    name { "John Smith" }
+    url { "http://example.com" }
+    avatar_url { "http://example.com/avatar" }
+    provider { "github" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    it 'should have valid factory' do
+      user = build :user
+      expect(user).to be_valid
+    end
+
+    it 'should validate presence of attributes' do
+      user = build :user, login: nil, provider: nil
+      expect(user).not_to be_valid
+      expect(user.errors.messages[:login]).to include("can't be blank")
+      expect(user.errors.messages[:provider]).to include("can't be blank")
+    end
+
+    it 'should validate uniqueness of login' do
+      user = create :user
+      other_user = build :user, login: user.login
+      expect(other_user).not_to be_valid
+      other_user.login = 'newlogin'
+      expect(other_user).to be_valid
+    end
+  end
 end
