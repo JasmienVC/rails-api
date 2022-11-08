8061feca6ded3db93dd941f57774c5b3ef0f5093
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-08 10:09:20 +0100

    replaced standard error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  include JsonapiErrorsHandler
+  ErrorMapper.map_errors!({ 'ActiveRecord::RecordNotFound' => 'JsonapiErrorHandler::Errors::NotFound' })
+  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
 end
