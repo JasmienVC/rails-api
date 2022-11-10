0c964fd01ab3c7c9434e663dc283982199192927
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-10 17:17:23 +0100

    added {} in spec file, because we passed block as argument

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -5,7 +5,7 @@ describe UserAuthenticator do
     context 'when code is incorrect' do
       it 'should raise an error' do
         authenticator = described_class.new('sample_code')
-        expect(authenticator.perform).to raise_error(
+        expect{ authenticator.perform }.to raise_error(
           UserAuthenticator::AuthenticationError
         )
         expect(authenticator.user).to be_nil
