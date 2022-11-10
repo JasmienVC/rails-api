12a6f3deb48fadb61ffc29ebe79572cae3426106
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-10 18:00:35 +0100

    working on verification

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -4,9 +4,18 @@ class UserAuthenticator
   attr_reader :user
 
   def initialize(code)
+    @code = code
   end
 
   def perform
-    raise AuthenticationError
+    client = Octokit::Client.new(
+      client_id: ENV['GITHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET']
+    )
+    res = client.exchange_code_for_token(@code)
+    if res.error.present?
+      raise AuthenticationError
+    else
+    end
   end
 end
--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -2,13 +2,28 @@ require 'rails_helper'
 
 describe UserAuthenticator do
   describe '#perform' do
+    let(:authenticator) { described_class.new('sample_code') }
+    subject { authenticator.perform }
     context 'when code is incorrect' do
+      let(:error) {
+        double("Sawyer::Resource", error: "bad_verification_code")
+      }
+      
+      before do
+        allow_any_intance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(error)
+      end
       it 'should raise an error' do
-        authenticator = described_class.new('sample_code')
-        expect{ authenticator.perform }.to raise_error(
+        expect{ subject }.to raise_error(
           UserAuthenticator::AuthenticationError
         )
         expect(authenticator.user).to be_nil
+      end
+    end
+
+    context 'when code is correct' do
+      it "should save the user if new" do
+        expect{ subject }.to change{ User.count }.by(1)
       end
     end
   end
