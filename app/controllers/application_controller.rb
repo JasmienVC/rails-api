868030eba9110b55dda23a07a1d3f6bf7b7a0559
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-11 17:33:33 +0100

    controller error tests are not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::API
-  include JsonapiErrorsHandler
-  ErrorMapper.map_errors!({ 'ActiveRecord::RecordNotFound' => 'JsonapiErrorHandler::Errors::NotFound' })
-  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+
+  private
+
+  def authentication_error
+    error = {
+      "status" => "401",
+      "source" => { "pointer" => "/code" },
+      "title" => "Invalid authentication code",
+      "detail" => "You must provide valid code in order to exchange it for token."
+    }
+    render json: { "errors": [error] }, status: 401
+  end
 end
--- spec/requests/access_tokens_spec.rb
+++ spec/requests/access_tokens_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "AccessTokens", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe AccessTokensController, type: :controller do
+  describe '#create' do
+    context 'when invalid request' do
+      let(:error) do
+        {
+          "status" => "401",
+          "source" => { "pointer" => "/code" },
+          "title" => "Invalid authentication code",
+          "detail" => "You must provide valid code in order to exchange it for token."
+        }
+      end
+
+      it 'should return 401 status' do
+        post :create
+        expect(response).to have_http_status(401)
+      end
+
+      it 'should return proper error body' do
+        post :create
+        expect(json["errors"]).to include(error)
+      end
+    end
+
+    context 'when valid request' do
+    end
   end
 end
