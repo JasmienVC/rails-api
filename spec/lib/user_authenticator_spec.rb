773da21d3457fa9d9cd8b02c025cbfae1f51a22d
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-11 11:55:28 +0100

    refactoring

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -5,16 +5,16 @@ describe UserAuthenticator do
     let(:authenticator) { described_class.new('sample_code') }
     subject { authenticator.perform }
     context 'when code is incorrect' do
-      let(:error) {
-        double("Sawyer::Resource", error: "bad_verification_code")
-      }
+      let(:error) { double('Sawyer::Resource', error: 'bad_verification_code') }
 
       before do
         allow_any_instance_of(Octokit::Client).to receive(
-          :exchange_code_for_token).and_return(error)
+          :exchange_code_for_token
+        ).and_return(error)
       end
+
       it 'should raise an error' do
-        expect{ subject }.to raise_error(
+        expect { subject }.to raise_error(
           UserAuthenticator::AuthenticationError
         )
         expect(authenticator.user).to be_nil
@@ -32,13 +32,22 @@ describe UserAuthenticator do
       end
       before do
         allow_any_instance_of(Octokit::Client).to receive(
-          :exchange_code_for_token).and_return('validaccesstoken')
+          :exchange_code_for_token
+        ).and_return('validaccesstoken')
         allow_any_instance_of(Octokit::Client).to receive(
-          :user).and_return(user_data)
+          :user
+        ).and_return(user_data)
       end
+
       it 'should save the user if new' do
-        expect { subject }.to change{ User.count }.by(1)
+        expect { subject }.to change { User.count }.by(1)
         expect(User.last.name).to eq('John Smith')
+      end
+
+      it 'should reuse already registered user' do
+        user = create :user, user_data
+        expect { subject }.not_to(change { User.count })
+        expect(authenticator.user).to eq(user)
       end
     end
   end
