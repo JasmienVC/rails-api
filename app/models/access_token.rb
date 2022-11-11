b49737e3645695b3d509f893a95c235b93480de9
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-11 13:04:22 +0100

    added tests upon initialization of new user

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,4 +1,15 @@
 class AccessToken < ApplicationRecord
   belongs_to :user
   validates :token, presence: true, uniqueness: true
+  after_initialize :generate_token
+
+  private
+
+  def generate_token
+    loop do
+      break if token.present? && AccessToken.exists?(token: token)
+      
+      self.token = SecureRandom.hex(10)
+    end
+  end
 end
--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe AccessToken, type: :model do
       expect(second_access_token).to be_valid
     end
   end
+
+  describe '#new' do
+    it 'should have a token present after initialize' do
+      expect(AccessToken.new.token).to be_present
+    end
+
+    it 'should generate unique token' do
+      user = create :user
+      expect { user.create_access_token }.to change { AccessToken.count }.by(1)
+      expect { user.build_access_token }.to be_valid
+    end
+  end
 end
